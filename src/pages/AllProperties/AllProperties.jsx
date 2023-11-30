686883e8840b4591d8a65d75f5e1486d698bd446
Md Shakil Hossain <mrshakilhossainofficial@gmail.com>
2023-11-30 23:09:19 +0600

    added short by view button

--- src/pages/AllProperties/AllProperties.jsx
+++ src/pages/AllProperties/AllProperties.jsx
@@ -1,17 +1,30 @@
 import { useEffect, useState } from "react";
+import { Helmet } from "react-helmet-async";
 import { getAllProperties } from "../../api/properties";
 import SectionTitle from "../../components/Shared/SectionTitle";
 import PropertyCard from "./PropertyCard";
-import { Helmet } from "react-helmet-async";
 
 const AllProperties = () => {
   const [properties, setProperties] = useState([]);
+  const [sortByPrice, setSortByPrice] = useState(false);
 
   useEffect(() => {
     getAllProperties().then((data) => setProperties(data));
   }, []);
 
-  const verifiedProperties = properties.filter(
+  const handleSortByPrice = () => {
+    setSortByPrice(!sortByPrice);
+  };
+
+  const sortedProperties = [...properties].sort((a, b) => {
+    if (sortByPrice) {
+      return a.price - b.price;
+    } else {
+      return b.price - a.price;
+    }
+  });
+
+  const verifiedProperties = sortedProperties.filter(
     (property) => property.status === "Verified"
   );
 
@@ -25,6 +38,9 @@ const AllProperties = () => {
           subHeading="Explore the All Available Property"
           heading="ALL PROPERTIES"
         ></SectionTitle>
+        <button className="px-3 py-1 text-md font-semibold bg-sky-600 text-white m-4 rounded-md" onClick={handleSortByPrice}>
+          Sort by Price {sortByPrice ? "(Low to High)" : "(High to Low)"}
+        </button>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
         {verifiedProperties.map((property) => (
